3746f6763e7edf9773056d8906432feeb2674465
scito <info@scito.ch>
2013-11-12 21:44:39 +0100

    Add prepare_release script: remove *.bak, change created_date timestamp column

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,4 +3,8 @@
 root_dir=public_html
 dir=$root_dir/settings
 
-rm `find $root_dir -name '*.bak'`
+rm `find $root_dir -name '*.bak'` -v
+
+cat ../data/lobbycontrol.sql \
+| perl -p -e's/timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP/timestamp NULL DEFAULT NULL/ig' \
+> ../data/prod_lobbycontrol.sql;
