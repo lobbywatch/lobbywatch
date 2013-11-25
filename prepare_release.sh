7675d0b8b29ada448eb7b3d5a85a3e4386ff4f64
scito <info@scito.ch>
2013-11-25 11:10:26 +0100

    Fix MySQL 5.1 incompatibilty: no comments for indexes

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,4 +3,6 @@
 cat ../data/lobbycontrol.sql \
 | perl -p -e's/timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP/timestamp NULL DEFAULT NULL/ig' \
 | perl -p -e's/`lobbycontrol`/`csvimsne_lobbycontrol`/ig' \
+| perl -p -e's/(UNIQUE KEY `\w*?` \(`\w*?`\)) COMMENT '.*?',/\1,/ig' \
+| perl -p -e's/COMMENT .Fachlicher unique constraint.//ig' \
 > ../data/prod_lobbycontrol.sql;
