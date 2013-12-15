3b397e08edc69f7132aa5d4c0664b626e3571a9e
scito <info@scito.ch>
2013-12-15 13:27:06 +0100

    Fix DEFINER statement which requires SUPER privilege

--- prepare_release.sh
+++ prepare_release.sh
@@ -8,4 +8,5 @@ cat ../data/lobbycontrol.sql \
 | perl -p -e's/(UNIQUE KEY `\w*?` \(`\w*?`\)) COMMENT '.*?',/\1,/ig' \
 | perl -p -e's/COMMENT .Fachlicher unique constraint.//ig' \
 | perl -p -e's/DEFINER=.*? SQL SECURITY DEFINER//ig' \
+| perl -p -e's/DEFINER=`root`@`localhost` //ig' \
 > ../data/deploy_lobbycontrol.sql;
