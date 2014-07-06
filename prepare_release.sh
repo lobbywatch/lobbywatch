6549f4a34dc045663c5354fbc9762aacfe035f0d
scito <info@scito.ch>
2014-07-06 22:11:41 +0200

    DB: Fix MySQL version workaround in deploy script for user table

--- prepare_release.sh
+++ prepare_release.sh
@@ -9,6 +9,7 @@ env_dir2_escaped=${env_dir2//\//\\\/}
 
 cat ../data/lobbywatch.sql \
 | perl -p -e's/timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP/timestamp NULL DEFAULT NULL/ig' \
+| perl -p -e's/timestamp NULL DEFAULT CURRENT_TIMESTAMP/timestamp NULL DEFAULT NULL/ig' \
 | perl -p -e"s/\`lobbywatch\`/\`csvimsne_lobbywatch$env_suffix\`/ig" \
 | perl -p -e's/(lobbywatch\.ch\/app\/)(files)/\1'''$env_dir_escaped'''\2/ig' \
 | perl -p -e's/(\/lobbywatch_db_files)/\1'''$env_dir2_escaped'''/ig' \
