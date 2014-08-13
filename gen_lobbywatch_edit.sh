bcc4cb10138eebf82c5e40838acb57720ed9831b
scito <info@scito.ch>
2014-08-13 10:57:43 +0200

    Forms: Use relative paths and fix lang customization

--- gen_lobbywatch_edit.sh
+++ gen_lobbywatch_edit.sh
@@ -20,4 +20,4 @@ do
   > "lobbywatch_bearbeitung_gen.pgtm";
 done
 
-wine "C:\Program Files (x86)\SQL Maestro Group\PHP Generator for MySQL Professional 12.8.0.18_20140722\MyPHPGeneratorPro.exe" "Z:\home\rkurmann\dev\web\lobbywatch\lobbydev\lobbywatch_bearbeitung_gen.pgtm" -output "Z:\home\rkurmann\dev\web\lobbywatch\lobbydev\public_html\bearbeitung" -generate
+wine "C:\Program Files (x86)\SQL Maestro Group\PHP Generator for MySQL Professional 12.8.0.18_20140722\MyPHPGeneratorPro.exe" "lobbywatch_bearbeitung_gen.pgtm" -output "public_html\bearbeitung" -generate
