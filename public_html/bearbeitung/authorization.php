5918cc16fde6a1edcef7ddc7d4c2c1b01db60949
scito <info@scito.ch>
2014-11-29 10:12:03 +0100

    Forms: Remove v_parlamentarier_authorisierungs_email form forms

--- public_html/bearbeitung/authorization.php
+++ public_html/bearbeitung/authorization.php
@@ -70,7 +70,6 @@ $tableCaptions = array('organisation' => '<span class="entity important-entity">
 'settings_category' => '<span class="settings">Settings Category</span>',
 'settings_category.settings' => '<span class="settings">Settings Category</span>.<s>Settings</s>',
 'user' => '<span class="settings">User</span>',
-'v_parlamentarier_authorisierungs_email' => '<span class="view">Parlamentarier Email</span>',
 'q_unvollstaendige_parlamentarier' => '<span class="view">Unvollständige Parlamentarier</span>',
 'q_unvollstaendige_zutrittsberechtigte' => '<span class="view">Unvollständige Zutrittsberechtigte</span>',
 'q_unvollstaendige_organisationen' => '<span class="view">Unvollständige Organisationen</span>',
