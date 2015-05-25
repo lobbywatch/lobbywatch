c2f15e49059149030b593378d6a4c605c7a40f66
scito <info@scito.ch>
2015-05-25 13:19:50 +0200

    DB: Add parlamentarier.homepage_2, add verwitwet, getrennt, increase title length

--- db_changes.sql
+++ db_changes.sql
@@ -2315,19 +2315,29 @@ INSERT INTO in_kommission (parlamentarier_id, kommission_id, von, funktion, parl
 
 ALTER TABLE `parlamentarier`
   ADD `parlament_number` INT NULL DEFAULT NULL COMMENT 'Number Feld auf ws.parlament.ch, wird von ws.parlament.ch importiert, wird z.B. als ID für Photos verwendet.' AFTER `parlament_biografie_id`,
-  ADD `titel` VARCHAR(50) NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert' AFTER `beruf_interessengruppe_id`,
+  ADD `titel` VARCHAR(75) NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert' AFTER `beruf_interessengruppe_id`,
   ADD `sprache` ENUM('de','fr','it', 'sk', 'rm') NULL DEFAULT NULL COMMENT 'Sprache des Parlamentariers, wird von ws.parlament.ch importiert' AFTER `facebook_name`,
   ADD `aemter` TEXT NULL DEFAULT NULL COMMENT 'Politische Ämter (importiert von ws.parlament.ch mandate)' AFTER `titel`,
   ADD `weitere_aemter` TEXT NULL DEFAULT NULL COMMENT 'Zusätzliche Ämter (importiert von ws.parlament.ch additionalMandate)' AFTER `aemter`,
+  ADD `homepage_2` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Zweite Homepage, importiert von ws.parlament.ch' AFTER `homepage`,
+  CHANGE `zivilstand` `zivilstand` ENUM('ledig','verheiratet','geschieden','eingetragene partnerschaft','getrennt', 'verwitwet') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Zivilstand',
   -- indexes
   ADD UNIQUE KEY `parlament_biografie_id_unique` (`parlament_biografie_id`);
 
 ALTER TABLE `parlamentarier_log`
   ADD `parlament_number` INT NULL DEFAULT NULL COMMENT 'Number Feld auf ws.parlament.ch, wird z.B. als ID für Photos verwendet.' AFTER `parlament_biografie_id`,
-  ADD `titel` VARCHAR(50) NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers' AFTER `beruf_interessengruppe_id`,
+  ADD `titel` VARCHAR(75) NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers' AFTER `beruf_interessengruppe_id`,
   ADD `sprache` ENUM('de','fr','it', 'sk', 'rm') NULL DEFAULT NULL COMMENT 'Sprache des Parlamentariers' AFTER `facebook_name`,
   ADD `aemter` TEXT NULL DEFAULT NULL COMMENT 'Politische Ämter (importiert von ws.parlament.ch mandate)' AFTER `titel`,
-  ADD `weitere_aemter` TEXT NULL DEFAULT NULL COMMENT 'Zusätzliche Ämter (importiert von ws.parlament.ch additionalMandate)' AFTER `aemter`;
+  ADD `weitere_aemter` TEXT NULL DEFAULT NULL COMMENT 'Zusätzliche Ämter (importiert von ws.parlament.ch additionalMandate)' AFTER `aemter`,
+  ADD `homepage_2` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Zweite Homepage, importiert von ws.parlament.ch' AFTER `homepage`,
+  CHANGE `zivilstand` `zivilstand` ENUM('ledig','verheiratet','geschieden','eingetragene partnerschaft','getrennt', 'verwitwet') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Zivilstand';
 
--- ALTER TABLE `parlamentarier` CHANGE `titel` `titel` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert';
--- ALTER TABLE `parlamentarier_log` CHANGE `titel` `titel` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert';
+-- ALTER TABLE `parlamentarier` CHANGE `titel` `titel` VARCHAR(75) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert';
+-- ALTER TABLE `parlamentarier_log` CHANGE `titel` `titel` VARCHAR(75) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Titel des Parlamentariers, wird von ws.parlament.ch importiert';
+
+-- ALTER TABLE `parlamentarier` ADD `homepage_2` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Zweite Homepage, importiert von ws.parlament.ch' AFTER `homepage`;
+-- ALTER TABLE `parlamentarier_log` ADD `homepage_2` VARCHAR(255) NULL DEFAULT NULL COMMENT 'Zweite Homepage, importiert von ws.parlament.ch' AFTER `homepage`;
+
+-- ALTER TABLE `parlamentarier` CHANGE `zivilstand` `zivilstand` ENUM('ledig','verheiratet','geschieden','eingetragene partnerschaft','getrennt', 'verwitwet') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Zivilstand';
+-- ALTER TABLE `parlamentarier_log` CHANGE `zivilstand` `zivilstand` ENUM('ledig','verheiratet','geschieden','eingetragene partnerschaft','getrennt', 'verwitwet') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Zivilstand';
