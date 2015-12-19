d706ef6133ab606514f11d685652c2f67c3afd1a
scito <info@scito.ch>
2015-12-19 10:27:55 +0100

    sh: Download baks to prod_bak in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -188,12 +188,15 @@ if $backup_db ; then
   ssh $ssh_user -t -p $ssh_port "cd $remote_db_dir$env_dir2; bash -c \"./run_db_script.sh csvimsne_lobbywatch$env_suffix csvimsne_script dbdump interactive\""
   echo "## Backup DB data"
   ssh $ssh_user -t -p $ssh_port "cd $remote_db_dir$env_dir2; bash -c \"./run_db_script.sh csvimsne_lobbywatch$env_suffix csvimsne_script dbdump_data interactive\""
+  echo "## Saved backups"
   ssh $ssh_user -t -p $ssh_port "cd $remote_db_dir$env_dir2; bash -c \"/bin/ls -hAlt bak | head -10\""
+  echo "## Download backup files to prod_bak"
+  rsync $verbose -avze "ssh -p $ssh_port" --include='*.sql.gz' --include='*.sql' --exclude '*' $dry_run $ssh_user:$remote_db_dir$env_dir2/bak/ prod_bak$env_dir2/
 fi
 
 if $refresh_viws ; then
   echo "## Copy DB views script"
-  include_db="--include db_views.sql --include db_check.sql  --include run_db_script.sh"
+  include_db="--include db_views.sql --include db_check.sql --include run_db_script.sh"
   rsync -avze "ssh -p $ssh_port" $include_db --exclude '*' --backup --backup-dir=bak $dry_run . $ssh_user:$remote_db_dir$env_dir2
 
   echo "## Run DB views script"
