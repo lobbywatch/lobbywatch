3dd5f80be35cd41b9e4ab64349801d176e3e6fe5
scito <info@scito.ch>
2016-01-03 08:08:38 +0100

    DB: Refactor DB procedures triggers: move both to db_procedures_triggers.sql

--- db_procedures_triggers.sql
+++ db_procedures_triggers.sql
@@ -1,3 +1,14 @@
+-- TODO update to 5.5 and rename trg_organisation_name_ins/trg_organisation_name_upd with before
+-- TODO include all log triggers
+-- TODO set visa in kommission propagation
+
+
+SET FOREIGN_KEY_CHECKS=0;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
+-- http://blog.mclaughlinsoftware.com/2012/07/03/placement-over-substance/
+SET SQL_MODE=(SELECT CONCAT(@@sql_mode,',IGNORE_SPACE'));
+
 -- PROCEDURES & FUNCTIONS
 
 -- Run: CALL takeSnapshot('roland', 'Initial');
@@ -10,8 +21,6 @@ BEGIN
   DECLARE sid int(11);
   INSERT INTO `snapshot` (`id`, `beschreibung`, `notizen`, `created_visa`, `created_date`, `updated_visa`, `updated_date`) VALUES (NULL, aBeschreibung, NULL, aVisa, ts, aVisa, ts);
   SELECT LAST_INSERT_ID() INTO sid;
-
-  -- A NEW DIFF
 
    INSERT INTO `branche_log`
      SELECT *, null, 'snapshot', null, ts, sid FROM `branche`;
@@ -513,3 +522,63 @@ BEGIN
 END;
 |
 DELIMITER ;
+
+-- TRIGGERS
+
+-- http://blog.mclaughlinsoftware.com/2012/07/03/placement-over-substance/
+SET SQL_MODE=(SELECT CONCAT(@@sql_mode,',IGNORE_SPACE'));
+
+-- Before MySQL 5.5
+drop trigger if exists trg_organisation_name_ins;
+delimiter //
+create trigger trg_organisation_name_ins before insert on organisation
+for each row
+thisTrigger: begin
+    IF @disable_triggers IS NOT NULL THEN LEAVE thisTrigger; END IF;
+    if new.name_de IS NULL AND new.name_fr IS NULL AND new.name_it IS NULL then
+        call organisation_name_de_fr_it_must_be_set;
+    end if;
+end
+//
+delimiter ;
+
+drop trigger if exists trg_organisation_name_upd;
+delimiter //
+create trigger trg_organisation_name_upd before update on organisation
+for each row
+thisTrigger: begin
+    IF @disable_triggers IS NOT NULL THEN LEAVE thisTrigger; END IF;
+    if new.name_de IS NULL AND new.name_fr IS NULL AND new.name_it IS NULL then
+        call organisation_name_de_fr_it_must_be_set;
+    end if;
+end
+//
+delimiter ;
+
+-- MySQL 5.5 required
+-- delimiter $$
+-- drop trigger if exists trg_organisation_name_ins $$
+-- create trigger trg_organisation_name_ins before insert on organisation
+-- for each row
+-- begin
+--     declare msg varchar(255);
+--     if new.name_de IS NULL AND new.name_fr IS NULL AND new.name_it IS NULL then
+--         set msg = concat('NameError: Either name_de, name_fr or name_it must be set. ID: ', cast(new.id as char));
+--         signal sqlstate '45000' set message_text = msg;
+--     end if;
+-- end $$
+-- delimiter ;
+-- delimiter $$
+-- drop trigger if exists trg_organisation_name_upd $$
+-- create trigger trg_organisation_name_upd before update on organisation
+-- for each row
+-- begin
+--     declare msg varchar(255);
+--     if new.name_de IS NULL AND new.name_fr IS NULL AND new.name_it IS NULL then
+--         set msg = concat('NameError: Either name_de, name_fr or name_it must be set. ID: ', cast(new.id as char));
+--         signal sqlstate '45000' set message_text = msg;
+--     end if;
+-- end $$
+-- delimiter ;
+
+-- Log triggers
