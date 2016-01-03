50b129f3c7367ec6bb1303a8365e2ec384b2b8db
scito <info@scito.ch>
2016-01-03 07:10:03 +0100

    sh: Add mode 'all' to db_prod_to_local.sh

--- db_prod_to_local.sh
+++ db_prod_to_local.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 # Copy production DB to local DB
-# Parameter 1: DBname or nothing (default DB = lobbywatchtest)
+# Parameter 1: DBname, all (copy to lobbywatchtest and lobbywatch) or nothing (default DB = lobbywatchtest)
 
 # ./db_prod_to_local.sh lobbywatch
 
 # Set defaut DB if no parameter given
-if [[ $1 ]]; then
+if [[ "$1" == "all" ]]; then
+  db=lobbywatchtest
+elif [[ $1 ]]; then
   db=$1
 else
   db=lobbywatchtest
 fi
 
 ./deploy.sh -b -p
+
 ./run_local_db_script.sh $db prod_bak/`cat prod_bak/last_dbdump_data.txt`
+
+if [[ "$1" == "all" ]]; then
+  db=lobbywatch
+  ./run_local_db_script.sh $db prod_bak/`cat prod_bak/last_dbdump_data.txt`
+fi
