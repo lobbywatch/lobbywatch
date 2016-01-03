399f1240070f0a3a0b9ca4df2c1104f37678a20d
scito <info@scito.ch>
2016-01-03 12:24:39 +0100

    sh: compare_local_db_structs.sh: filter known differences

--- compare_local_db_structs.sh
+++ compare_local_db_structs.sh
@@ -2,6 +2,10 @@
 
 # ./run_local_db_script.sh lobbywatchtest db_procedures.sql ; ./compare_local_db_structs.sh -x
 
+# \s*-- .*
+
+# http://unix.stackexchange.com/questions/17040/how-to-diff-files-ignoring-comments-lines-starting-with
+
 . common.sh
 
 verbose_mode=false
@@ -39,14 +43,21 @@ done
 
 ./run_local_db_script.sh lobbywatch dbdump_struct
 db1_struct=`cat last_dbdump_file.txt`
+db1_struct_tmp=/tmp/$db1_struct
+mkdir -p `dirname $db1_struct_tmp`
+grep -vE '^\s*(\/\*!50003 SET (sql_mode|character_set_client|character_set_results|collation_connection))|FOR EACH ROW thisTrigger: begin$|FOR EACH ROW$|for each row\s*$|thisTrigger: begin\s*$|thisTrigger: BEGIN$|--\s+' $db1_struct > $db1_struct_tmp
 
 ./run_local_db_script.sh lobbywatchtest dbdump_struct
 db2_struct=`cat last_dbdump_file.txt`
+db2_struct_tmp=/tmp/$db2_struct
+mkdir -p `dirname $db2_struct_tmp`
+grep -vE '^\s*(\/\*!50003 SET (sql_mode|character_set_client|character_set_results|collation_connection))|FOR EACH ROW thisTrigger: begin$|FOR EACH ROW$|for each row\s*$|thisTrigger: begin\s*$|thisTrigger: BEGIN$|--\s+' $db2_struct > $db2_struct_tmp
 
-echo "diff -u -w $db1_struct $db2_struct | less"
+echo "diff -u -w $db1_struct_tmp $db2_struct_tmp | less"
 
+# grep -vE '^\s*(\/\*!50003 SET sql_mode)' `cat last_dbdump_file.txt`
 if $visual ; then
-  kompare $db1_struct $db2_struct
+  kompare $db1_struct_tmp $db2_struct_tmp &
 else
-  diff -u -w $db1_struct $db2_struct | less
+  diff -u -w $db1_struct_tmp $db2_struct_tmp | less
 fi
