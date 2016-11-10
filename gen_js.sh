22105d3778bd0cd0860704af986bf771401cc67c
scito <info@scito.ch>
2016-11-10 11:20:26 +0100

    fix(sh): fix gen_js -d

--- gen_js.sh
+++ gen_js.sh
@@ -8,7 +8,7 @@ r.js -o require-build-custom.js
 # baseUrl=$js_dir out=$js_dir/main-bundle-custom.js
 
 # Debug mode
-if  [[ "$1" != "-d" ]] ; then
+if  [[ "$1" == "-d" ]] ; then
     # Create files which are split at define( and function in order to be able to compare the files
     cat $js_dir/main-bundle.js | sed 's/\(define(\|function\)/\n\1/g' > $js_dir/main-bundle-split.js
     cat $js_dir/main-bundle-custom.js | sed 's/\(define(\|function\)/\n\1/g' > $js_dir/main-bundle-custom-split.js
