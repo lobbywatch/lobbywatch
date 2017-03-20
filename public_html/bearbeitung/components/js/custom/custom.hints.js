b3adf42bb2928e025cc018619b1269fcaedc97f1
scito <info@scito.ch>
2017-03-20 15:05:37 +0100

    fix(forms): fix hints/tooltips (require minify problem)

--- public_html/bearbeitung/components/js/custom/custom.hints.js
+++ public_html/bearbeitung/components/js/custom/custom.hints.js
@@ -1,23 +1,22 @@
-define([
-    'custom/jquery-ui'
-], function(tabs) {
+$(function() {
 
-    // Copied from pgui.grid.js
-    $("div[data-hint], img[data-hint], th[data-hint]").each(function() {
-//         console.log('custom.hints each');
-        // http://getbootstrap.com/javascript/#tooltips
-        // http://getbootstrap.com/javascript/#popovers
-        $(this).popover({
-            placement: 'right',
-            container: 'body',
-            trigger: 'hover',
-            html: true,
-            title: $(this).attr('data-hinttitle'),
-            content: $(this).attr('data-hint')
-        })
-        // http://stackoverflow.com/questions/19448902/changing-the-width-of-bootstrap-popover
+  // console.log('custom.hints init');
+
+  // Copied from pgui.grid.js
+  $("div[data-hint], img[data-hint], th[data-hint]").each(function() {
+      // console.log('custom.hints each');
+      // http://getbootstrap.com/javascript/#tooltips
+      // http://getbootstrap.com/javascript/#popovers
+      $(this).popover({
+          placement: 'right',
+          container: 'body',
+          trigger: 'hover',
+          html: true,
+          title: $(this).attr('data-hinttitle'),
+          content: $(this).attr('data-hint')
+      })
+      // http://stackoverflow.com/questions/19448902/changing-the-width-of-bootstrap-popover
 //        .on("show.bs.popover", function(){ $(this).data("bs.popover").tip().css("max-width", "600px"); })
-        ;
-    });
-
+      ;
+  });
 });
