2d742d305a4fca74ab6f2c00d6d4e955d0c30897
scito <info@scito.ch>
2017-03-20 15:09:15 +0100

    feat(sh): show sha1 hash changes in script log for gen_css.sh and gen_js.sh

--- gen_css.sh
+++ gen_css.sh
@@ -4,6 +4,8 @@ root_dir=public_html
 dir=$root_dir/bearbeitung
 assets_dir=$dir/components/assets
 main_css=$assets_dir/css/main.css
+bundle_hash_file_main=$root_dir/custom/hash_css_main.php
+bundle_hash_file_custom=$root_dir/custom/hash_css_custom.php
 
 # https://www.sqlmaestro.com/products/mysql/phpgenerator/help/02_04_00_style_sheets_internals/
 # https://github.com/dotless/dotless/wiki/Using-.less
@@ -16,5 +18,11 @@ mv $main_css ${main_css}.bak
 #echo $WINE "$WINE_LESS -r $assets_dir/less/main.less $main_css"
 $WINE "$WINE_LESS" -r "$@" $assets_dir/less/main.less $main_css
 
-echo -e "<?php\n\$hash_css_main = '`sha1sum $main_css | cut -c -7`';" > $root_dir/custom/hash_css_main.php
-echo -e "<?php\n\$hash_css_custom = '`sha1sum $assets_dir/css/custom/custom.css | cut -c -7`';" > $root_dir/custom/hash_css_custom.php
+mv $bundle_hash_file_main $bundle_hash_file_main.bak
+echo -e "<?php\n\$hash_css_main = '`sha1sum $main_css | cut -c -7`';" > $bundle_hash_file_main
+diff -u0 $bundle_hash_file_main.bak $bundle_hash_file_main | tail -2
+
+mv $bundle_hash_file_custom $bundle_hash_file_custom.bak
+echo -e "<?php\n\$hash_css_custom = '`sha1sum $assets_dir/css/custom/custom.css | cut -c -7`';" > $bundle_hash_file_custom
+diff -u0 $bundle_hash_file_custom.bak $bundle_hash_file_custom | tail -2
+
