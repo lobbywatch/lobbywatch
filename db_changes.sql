13bab6bd62798058ecd1acbfde368b181867c060
scito <info@scito.ch>
2017-05-07 14:21:09 +0200

    refactor(DB): move db scraper script

--- db_changes.sql
+++ db_changes.sql
@@ -2809,3 +2809,36 @@ ADD `updated_by_import` timestamp NULL DEFAULT NULL COMMENT 'Datum, wann die Zut
 
 ALTER TABLE zutrittsberechtigung_log
 ADD `updated_by_import` timestamp NULL DEFAULT NULL COMMENT 'Datum, wann die Zutrittsberechtigung durch einen Import zu letzt aktualisiert wurde.' AFTER bis;
+
+
+-- 10.04.2017
+
+-- Die SP heisst im PDF auf Französisch abgekürzt PSS und nicht PS, drum:
+UPDATE `partei`
+SET `abkuerzung_fr` = 'PSS'
+WHERE `id` = 3;
+
+-- Die MCG (https://de.wikipedia.org/wiki/Mouvement_citoyens_genevois) heisst in der datenbank MCR. Sie nennt sich scheinbar selbst aber MCG, darum:
+UPDATE `partei`
+SET `abkuerzung` = 'MCG'
+WHERE `id` = 9;
+
+-- Die PdA heisst auf französisch PdT, drum:
+UPDATE `partei`
+SET `abkuerzung_fr` = 'PdT'
+WHERE `id` = 13;
+
+-- Der Mensch heisst Bernhard, nicht Bernard (Siehe http://www.sgv-usam.ch/verband/geschaeftsstelle/team.html)
+UPDATE `person`
+SET `vorname` = 'Bernhard'
+WHERE `id` = 207;
+
+-- Der Mensch wird im PDF als "Ruedi" ausgewiesen, drum kommt hier analog zu anderen der Spitzname dazu (vorher war nur "Rudolf")
+UPDATE `person`
+SET `vorname` = 'Rudolf "Ruedi"'
+WHERE `id` = 121;
+
+-- Der Mensch wird im PDF als "Tim" ausgewiesen, drum kommt hier analog zu anderen der Spitzname dazu (vorher war nur "Timotheos")
+UPDATE `person`
+SET `vorname` = 'Timotheos "Tim"'
+WHERE `id` = 269;
