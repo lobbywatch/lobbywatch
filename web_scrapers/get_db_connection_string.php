0764c899e5879b5a9ce4c5053d272687fdc75a44
scito <info@scito.ch>
2017-07-14 16:37:11 +0200

    fix(zb): use reader_user instead of full featured user

--- web_scrapers/get_db_connection_string.php
+++ web_scrapers/get_db_connection_string.php
@@ -1,4 +1,4 @@
 <?php
 require_once dirname(__FILE__) . '/../public_html/settings/settings.php';
 
-print("${db_connection["username"]}:${db_connection["password"]}:${db_connection["server"]}:${db_connection["database"]}:${db_connection["port"]}");
+print("${db_connection["reader_username"]}:${db_connection["reader_password"]}:${db_connection["server"]}:${db_connection["database"]}:${db_connection["port"]}");
