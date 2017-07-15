fb3e5bee86a38d37c959b9ef57b8d0e0e3f8ae9b
scito <info@scito.ch>
2017-07-15 21:15:06 +0200

    refactor(sh): add metadata for import_status.php

--- import_status.php
+++ import_status.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 <title><?php print($_SERVER['HTTP_HOST']); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">
 </head>
 <body>
 <h1><?php print($_SERVER['HTTP_HOST']); ?></h1>
