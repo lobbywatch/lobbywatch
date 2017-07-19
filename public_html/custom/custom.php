5c44b0da7c3320b6776a1147ee7eb2ded776317d
scito <info@scito.ch>
2017-07-19 06:59:22 +0200

    feat(forms): Show DB in forms footer

--- public_html/custom/custom.php
+++ public_html/custom/custom.php
@@ -2174,10 +2174,10 @@ function getCustomPagesHeader() {
 }
 
 function getCustomPagesFooter() {
-  global $env, $env_dir, $env_dirauswertung, $version, $generator_version, $deploy_date, $build_date, $import_date_wsparlamentch;
+  global $env, $env_dir, $env_dirauswertung, $version, $generator_version, $deploy_date, $build_date, $import_date_wsparlamentch, $db_connection;
   return "Bearbeitungsseiten von <a href='$env_dir'>Lobbywatch $env</a>;
   <!-- a href='$env_dirauswertung'>Auswertung</a--> <a href='/wiki'>Wiki</a><br>
-  Mode: $env / Version: $version / Generator: $generator_version / Deploy date: $deploy_date: / Build date: $build_date: /
+  Mode: $env / DB: {$db_connection['database']} / Version: $version / Generator: $generator_version / Deploy date: $deploy_date: / Build date: $build_date: /
   Last ws.parlament.ch import: $import_date_wsparlamentch / Page execution time: " . _custom_page_build_secs() . "s";
 }
 
