335e9c9adf8864d884769f8531342c08a910c8cd
scito <info@scito.ch>
2018-06-28 16:33:07 +0900

    docs(forms): add initial document for forms upgrade to PHPGen 18.3.0.2

---
+++ UPG_TODO.php
@@ -0,0 +1,38 @@
+<?
+// Upgrage Todos UPG
+
+/*
+TODO Migrate Sources to UTF-8
+TODO Find latin-1 files
+TODO check custom files for changes
+TODO SelectedOperationGridState migration
+TODO remove convert_utf8() calls
+TODO remove trailing whitespace in generated code, https://stackoverflow.com/questions/9532340/how-do-i-remove-trailing-whitespace-using-a-regular-expression
+TODO Remove calls to $this->RenderText()
+TODO Remove ^M in generated *.js files
+TODO Update custom_templates
+TODO Check utils.createLoadingModalDialog(localizer.getString('Deleting')).modal();
+TODO Use public_html/bearbeitung/components/common_utils.php
+TODO Delete all public_html/bearbeitung files and detect unnecessary files
+TODO Afterburner which necessary, clean up
+TODO export lobbywatch.sql
+TODO improve custom/custom_page.php
+
+*/
+
+/*
+
+## Custom features added to PHPGen framework
+
+* Comments on edit forms
+* Bulk Ops [rework]
+* Enhanced comments on grid header
+* Call UID WS
+* Marked fields on edit/insert form
+* Main.css
+* Main.js
+* Preview
+* Default filter out retired parlamentarier
+* Improved security [rework]
+
+*/
