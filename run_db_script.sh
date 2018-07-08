557f5cd763030370a9c558eff62dd910083b31bb
scito <info@scito.ch>
2018-07-08 22:55:34 +0900

    ref(DB): add drop table and trigger for full db dump

--- run_db_script.sh
+++ run_db_script.sh
@@ -96,7 +96,7 @@ if [[ "$script" == "dbdump" ]] ; then
   # --add-drop-database --routines --skip-extended-insert
   # Add --skip-quote-names http://www.iheavy.com/2012/08/09/5-things-you-overlooked-with-mysql-dumps/
   # http://unix.stackexchange.com/questions/20573/sed-insert-something-to-the-last-line
-  (set -o pipefail; mysqldump -h $HOST -u$username $PW --databases $db --dump-date --hex-blob --complete-insert --skip-lock-tables --single-transaction --routines --log-error=$logfile 2>>$logfile |
+  (set -o pipefail; mysqldump -h $HOST -u$username $PW --databases $db --dump-date --hex-blob --complete-insert --skip-lock-tables --single-transaction --routines --add-drop-table --add-drop-trigger --log-error=$logfile 2>>$logfile |
    sed -r "s/^\s*USE.*;/-- Created: `date +"%d.%m.%Y %T"`\n\n\0\n\nSET @disable_triggers = 1; -- ibex disable triggers/i" |
    sed -e "\$aSET @disable_triggers = NULL; -- ibex enable triggers" |
    perl -p -e's/DEFINER=.*? SQL SECURITY DEFINER//ig' |
