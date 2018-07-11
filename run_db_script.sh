4f1e6c4393eeceb8603446d7b15de535c0ddd7b6
scito <info@scito.ch>
2018-07-11 23:01:18 +0900

    fix(docker): set utf-8 charset for mysql clients in the container which do not have locales properly set

--- run_db_script.sh
+++ run_db_script.sh
@@ -44,8 +44,9 @@ MYSQL_CONTAINER=mysql57
 
 docker exec -it $MYSQL_CONTAINER mysql --help >/dev/null 2>&1 && IS_DOCKER=true || IS_DOCKER=false
 if $IS_DOCKER && [ "$PORT" == "3306" ]; then
-  MYSQLDUMP="docker exec -it $MYSQL_CONTAINER mysqldump"
-  MYSQL="docker exec -i $MYSQL_CONTAINER mysql"
+  # Set the default char-set since it may not be set in the docker container
+  MYSQLDUMP="docker exec -it $MYSQL_CONTAINER mysqldump --default-character-set=utf8"
+  MYSQL="docker exec -i $MYSQL_CONTAINER mysql --default-character-set=utf8"
 else
   MYSQLDUMP=mysqldump
   MYSQL=mysql
