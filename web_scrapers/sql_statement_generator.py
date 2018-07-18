53a55ffb5b61c09fc29e87abfad20b1388a3c9fa
scito <info@scito.ch>
2018-07-18 17:56:39 +0900

    ref(pg): SQL statements on one line for more compact logging

--- web_scrapers/sql_statement_generator.py
+++ web_scrapers/sql_statement_generator.py
@@ -7,32 +7,7 @@ user = os.getenv('USER', 'import')
 
 # create new zutrittsberechtigung for a perlamentarier
 def insert_zutrittsberechtigung(parlamentarier_id, person_id, funktion, date):
-    query = """
-    INSERT INTO `zutrittsberechtigung` (
-        `parlamentarier_id`, 
-        `person_id`, 
-        `funktion`, 
-        `von`, 
-        `notizen`, 
-        `created_visa`, 
-        `created_date`, 
-        `updated_visa`, 
-        `updated_date`, 
-        `updated_by_import`)
-    VALUES (
-        {0}, 
-        {1}, 
-        '{2}', 
-        STR_TO_DATE('{3}', '%d.%m.%Y'), 
-        '{4}', 
-        '{5}', 
-        STR_TO_DATE('{6}', 
-        '%d.%m.%Y %T'), 
-        '{7}', 
-        STR_TO_DATE('{8}', '%d.%m.%Y %T'), 
-        STR_TO_DATE('{8}', '%d.%m.%Y %T')
-        );
-        """.format(
+    query = "INSERT INTO `zutrittsberechtigung` (`parlamentarier_id`, `person_id`, `funktion`, `von`, `notizen`, `created_visa`, `created_date`, `updated_visa`, `updated_date`, `updated_by_import`) VALUES ({0}, {1}, '{2}', STR_TO_DATE('{3}', '%d.%m.%Y'), '{4}', '{5}', STR_TO_DATE('{6}', '%d.%m.%Y %T'), '{7}', STR_TO_DATE('{8}', '%d.%m.%Y %T'), STR_TO_DATE('{8}', '%d.%m.%Y %T'));\n".format(
             parlamentarier_id,
             person_id if person_id is not None else "(SELECT LAST_INSERT_ID())",
             funktion,
@@ -49,15 +24,7 @@ def insert_zutrittsberechtigung(parlamentarier_id, person_id, funktion, date):
 
 # update the function of an existing zutrittsberechtigung
 def update_function_of_zutrittsberechtigung(zutrittsberechtigung_id, function, date):
-    query = """
-    UPDATE `zutrittsberechtigung`
-    SET `funktion` = '{0}', 
-    `notizen` = CONCAT_WS('{1}', notizen), 
-    `updated_visa` = '{2}', 
-    `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), 
-    `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T')
-    WHERE `id` = {4};
-    """.format(
+    query = "UPDATE `zutrittsberechtigung` SET `funktion` = '{0}', `notizen` = CONCAT_WS('{1}', notizen), `updated_visa` = '{2}', `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T') WHERE `id` = {4};\n".format(
         _escape_string(function),
         "{0}/import/{1}: Funktion geändert\\n\\n".format(
             _date_as_sql_string(date), user),
@@ -71,15 +38,7 @@ def update_function_of_zutrittsberechtigung(zutrittsberechtigung_id, function, d
 
 # end a zutrittsberechtigung
 def end_zutrittsberechtigung(zutrittsberechtigung_id, date):
-    query = """
-    UPDATE `zutrittsberechtigung`
-    SET `bis` = STR_TO_DATE('{0}', '%d.%m.%Y'), 
-    `notizen` = CONCAT_WS('{1}', notizen), 
-    `updated_visa` = '{2}', 
-    `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), 
-    `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T')
-    WHERE `id` = {4};
-    """.format(
+    query = "UPDATE `zutrittsberechtigung` SET `bis` = STR_TO_DATE('{0}', '%d.%m.%Y'), `notizen` = CONCAT_WS('{1}', notizen), `updated_visa` = '{2}', `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T') WHERE `id` = {4};\n".format(
         _date_as_sql_string(date),
         "{0}/import/{1}: Bis-Datum gesetzt\\n\\n".format(
             _date_as_sql_string(date), user),
@@ -93,29 +52,7 @@ def end_zutrittsberechtigung(zutrittsberechtigung_id, date):
 
 # insert a new person
 def insert_person(guest, date):
-    query = """
-    INSERT INTO `person` (
-        `nachname`, 
-        `vorname`, 
-        `zweiter_vorname`, 
-        `beschreibung_de`, 
-        `created_visa`, 
-        `created_date`, 
-        `updated_visa`, 
-        `updated_date`, 
-        `updated_by_import`, 
-        `notizen`)
-    VALUES (
-        '{0}', 
-        '{1}', 
-        '{2}', 
-        '{3}', 
-        '{4}', 
-        STR_TO_DATE('{5}', '%d.%m.%Y %T'), 
-        '{6}', 
-        STR_TO_DATE('{7}', '%d.%m.%Y %T'), 
-        STR_TO_DATE('{7}', '%d.%m.%Y %T'), 
-        '{8}');""".format(
+    query = "INSERT INTO `person` (`nachname`, `vorname`, `zweiter_vorname`, `beschreibung_de`, `created_visa`, `created_date`, `updated_visa`, `updated_date`, `updated_by_import`, `notizen`) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', STR_TO_DATE('{5}', '%d.%m.%Y %T'), '{6}', STR_TO_DATE('{7}', '%d.%m.%Y %T'), STR_TO_DATE('{7}', '%d.%m.%Y %T'), '{8}');\n".format(
             _escape_string(guest["names"][0]),
             _escape_string(guest["names"][1]),
             _escape_string(guest["names"][2] if len(
@@ -133,36 +70,9 @@ def insert_person(guest, date):
 
 # insert a new organisation with the characteristics of a parlamentarische gruppe
 def insert_parlamentarische_gruppe(name_de, sekretariat, homepage, date):
-    query = """
-    INSERT INTO `organisation` (
-        `name_de`, 
-        `sekretariat`,
-        `homepage`,
-        `land_id`, 
-        `rechtsform`, 
-        `typ`, 
-        `vernehmlassung`, 
-        `created_visa`, 
-        `created_date`, 
-        `updated_visa`, 
-        `updated_by_import`, 
-        `notizen`)
-    VALUES (
-        '{}', 
-        '{}', 
-        '{}', 
-        {}, 
-        '{}', 
-        '{}', 
-        '{}', 
-        '{}', 
-        STR_TO_DATE('{}', '%d.%m.%Y %T'), 
-        '{}', 
-        STR_TO_DATE('{}', '%d.%m.%Y %T'), 
-        '{}');
-
-        SET @last_parlamentarische_gruppe = LAST_INSERT_ID();
-        """.format(
+    query = """INSERT INTO `organisation` (`name_de`, `sekretariat`,`homepage`,`land_id`, `rechtsform`, `typ`, `vernehmlassung`, `created_visa`, `created_date`, `updated_visa`, `updated_by_import`, `notizen`) VALUES ('{}', '{}', '{}', {}, '{}', '{}', '{}', '{}', STR_TO_DATE('{}', '%d.%m.%Y %T'), '{}', STR_TO_DATE('{}', '%d.%m.%Y %T'), '{}');
+SET @last_parlamentarische_gruppe = LAST_INSERT_ID();
+""".format(
             _escape_string(name_de),
             _escape_string(sekretariat),
             _escape_string(homepage),
@@ -181,15 +91,7 @@ def insert_parlamentarische_gruppe(name_de, sekretariat, homepage, date):
 
 
 def update_sekretariat_organisation(organisation_id, sekretariat, batch_time):
-    query = """
-    UPDATE `organisation`
-    SET `sekretariat` = '{0}', 
-    `notizen` = CONCAT_WS('{1}', notizen), 
-    `updated_visa` = '{2}', 
-    `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), 
-    `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T')
-    WHERE `id` = {4};
-    """.format(
+    query = "UPDATE `organisation` SET `sekretariat` = '{0}', `notizen` = CONCAT_WS('{1}', notizen), `updated_visa` = '{2}', `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T') WHERE `id` = {4};\n".format(
         _escape_string(sekretariat),
         "{0}/import/{1}: Sekretariat geändert\\n\\n".format(
             _date_as_sql_string(batch_time), user),
@@ -202,15 +104,7 @@ def update_sekretariat_organisation(organisation_id, sekretariat, batch_time):
 
 
 def update_homepage_organisation(organisation_id, homepage, batch_time):
-    query = """
-    UPDATE `organisation`
-    SET `homepage` = '{0}', 
-    `notizen` = CONCAT_WS('{1}', notizen), 
-    `updated_visa` = '{2}', 
-    `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), 
-    `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T')
-    WHERE `id` = {4};
-    """.format(
+    query = "UPDATE `organisation` SET `homepage` = '{0}', `notizen` = CONCAT_WS('{1}', notizen), `updated_visa` = '{2}', `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T') WHERE `id` = {4};\n".format(
         _escape_string(homepage),
         "{0}/import/{1}: Homepage geändert\\n\\n".format(
             _date_as_sql_string(batch_time), user),
@@ -224,38 +118,7 @@ def update_homepage_organisation(organisation_id, homepage, batch_time):
 def insert_interessenbindung_parlamentarische_gruppe(parlamentarier_id,
                                                      organisation_id, stichdatum, beschreibung, date):
 
-    query = """
-    INSERT INTO `interessenbindung` (
-        `parlamentarier_id`, 
-        `organisation_id`, 
-        `art`, 
-        `funktion_im_gremium`, 
-        `beschreibung`,
-        `deklarationstyp`, 
-        `status`, 
-        `behoerden_vertreter`, 
-        `von`, 
-        `created_visa`,
-        `created_date`, 
-        `updated_visa`, 
-        `updated_by_import`, 
-        `notizen`) 
-    VALUES (
-        {}, 
-        {}, 
-        '{}', 
-        '{}', 
-        '{}',
-        '{}', 
-        '{}', 
-        '{}', 
-        STR_TO_DATE('{}', '%d.%m.%Y'),
-        '{}',
-        STR_TO_DATE('{}', '%d.%m.%Y %T'), 
-        '{}', 
-        STR_TO_DATE('{}', '%d.%m.%Y %T'), 
-        '{}');
-    """.format(
+    query = "INSERT INTO `interessenbindung` (`parlamentarier_id`, `organisation_id`, `art`, `funktion_im_gremium`, `beschreibung`,`deklarationstyp`, `status`, `behoerden_vertreter`, `von`, `created_visa`,`created_date`, `updated_visa`, `updated_by_import`, `notizen`) VALUES ({}, {}, '{}', '{}', '{}','{}', '{}', '{}', STR_TO_DATE('{}', '%d.%m.%Y'),'{}',STR_TO_DATE('{}', '%d.%m.%Y %T'), '{}', STR_TO_DATE('{}', '%d.%m.%Y %T'), '{}');\n".format(
         parlamentarier_id,
         organisation_id,
         "vorstand",
@@ -275,15 +138,7 @@ def insert_interessenbindung_parlamentarische_gruppe(parlamentarier_id,
 
 
 def end_interessenbindung(interessenbindung_id, stichdatum, batch_time):
-    query = """
-    UPDATE `interessenbindung`
-    SET `bis` = STR_TO_DATE('{0}', '%d.%m.%Y'), 
-    `notizen` = CONCAT_WS('{1}', notizen), 
-    `updated_visa` = '{2}', 
-    `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), 
-    `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T')
-    WHERE `id` = {4};
-    """.format(
+    query = "UPDATE `interessenbindung` SET `bis` = STR_TO_DATE('{0}', '%d.%m.%Y'), `notizen` = CONCAT_WS('{1}', notizen), `updated_visa` = '{2}', `updated_date` = STR_TO_DATE('{3}', '%d.%m.%Y %T'), `updated_by_import` = STR_TO_DATE('{3}', '%d.%m.%Y %T') WHERE `id` = {4};\n".format(
         _date_as_sql_string(stichdatum),
         "{0}/import/{1}: Bis-Datum gesetzt\\n\\n".format(
             _date_as_sql_string(batch_time), user),
@@ -293,7 +148,6 @@ def end_interessenbindung(interessenbindung_id, stichdatum, batch_time):
     )
 
     return query
-
 
 
 # simple esape function for input strings
