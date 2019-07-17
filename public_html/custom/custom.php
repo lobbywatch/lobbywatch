554730935827909ccf2f88a4e5f076ddae81fd78
scito <info@scito.ch>
2019-07-17 09:21:54 +0200

    fix(forms): fix parlamentarier_overview.php group_concat broken

--- public_html/custom/custom.php
+++ public_html/custom/custom.php
@@ -2287,7 +2287,7 @@ function customGetPageInfos(array $pageInfos) {
 
 // Must not be in utils.php since in Drupal we use a similar function
 function set_db_session_parameters($con) {
-  $session_sql = "SET SESSION group_concat_max_len=10000;" .
+  $session_sql = "SET SESSION group_concat_max_len=50000;" .
     "SET sql_mode='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';";
   $con->query($session_sql);
 }
