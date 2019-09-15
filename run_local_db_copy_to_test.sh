89986b3e2b5fd480fa340cf047306c8cce7a7727
scito <info@scito.ch>
2019-09-15 10:44:54 +0200

    fix(sh): use script DB user instead of root in run_local_db_copy_to_test.sh

--- run_local_db_copy_to_test.sh
+++ run_local_db_copy_to_test.sh
@@ -47,7 +47,7 @@ else
 fi
 
 
-user=root
+user=script
 
 # $MYSQLDUMP -u $user --skip-extended-insert --dump-date --hex-blob --routines --databases lobbywatch --add-drop-database > /tmp/db_out.sql
 $MYSQL -u $user -e "DROP DATABASE IF EXISTS $db_dest; CREATE DATABASE IF NOT EXISTS $db_dest DEFAULT CHARACTER SET $charset;"
