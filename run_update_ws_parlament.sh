55624a6e0dece153730477e9bdef377fe1e74921
scito <info@scito.ch>
2020-03-21 10:51:51 +0100

    fix(zb,pg): create backup and archive if not exist

--- run_update_ws_parlament.sh
+++ run_update_ws_parlament.sh
@@ -341,6 +341,7 @@ if ! $nozb ; then
   if ! $automatic ; then
     askContinueYn "Run zutrittsberechtigten (zb) python for '$db' on '$HOSTNAME'?"
   fi
+  mkdir backup archive
   echo "Writing zb.json..."
   python3 $zb_script_path/zb_create_json.py
   echo "Writing zb_delta.sql based on $db..."
@@ -371,6 +372,7 @@ if ! $nopg ; then
   if ! $automatic ; then
     askContinueYn "Run parlamentarische Gruppen (pg) python '$db' on '$HOSTNAME'?"
   fi
+  mkdir backup archive
   echo "Writing pg.json..."
   if $lastpdf ; then
     last_pg_pdf=$(ls -t web_scrapers/backup/*gruppen.pdf | head -1)
