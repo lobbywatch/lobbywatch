f6f441dcb224caed49da29a9921966458a9f5df7
scito <info@scito.ch>
2020-12-30 11:29:43 +0100

    fix(utils): remove spaces and more unicode chars in clean_str()

--- public_html/common/utils.php
+++ public_html/common/utils.php
@@ -3891,7 +3891,9 @@ function clean_str(?string $str): ?string {
   if (!isset($str)) return null;
   $cleaned = Normalizer::normalize($str, Normalizer::FORM_C);
   // replace typographic chars
-  return trim(preg_replace(['%[«»“”„]%ui', '%[`‘’‚]%ui', '%[-–—]+%ui', '%[®]%ui'], ['"', "'", "-"], $cleaned));
+  // https://stackoverflow.com/questions/26458654/regular-expressions-for-a-range-of-unicode-points-php
+  // https://www.compart.com/en/unicode/block/U+2000
+  return trim(preg_replace(['%[\x{201C}-\x{201F}«»“”„]%ui', '%[\x{2018}-\x{201B}`‘’‚‹›]%ui', '%[-\x{2010}-\x{2014}\x{202F}]%ui', "%[ \x{2000}-\x{200A}]%ui", '%[®\x{2028}\x{2029}\x{200B}\x{2063}]%ui'], ['"', "'", "-", " " , ''], $cleaned));
 }
 
 /**
