887fdf8dc966e46978df595eaaed0fc609f5cc50
scito <info@scito.ch>
2023-10-21 18:13:18 +0200

    fix(pg/zb): replace deprected PdfFileReader by PdfReader

--- web_scrapers/pdf_helpers.py
+++ web_scrapers/pdf_helpers.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 import re
 import requests
 from datetime import datetime
@@ -7,21 +7,16 @@ from datetime import datetime
 # Returns creation date of PDF
 def extract_creation_date(filename):
     #  Add strict=False in order to avoid 'PdfReadWarning: Xref table not zero-indexed. ID numbers for objects will be corrected. [pdf.py:1736]'
-    pdf_toread = PdfFileReader(open(filename, "rb"), strict=False)
+    pdf_toread = PdfReader(open(filename, "rb"), strict=False)
     # "file has not been decrypted" error https://github.com/mstamy2/PyPDF2/issues/51
-    if pdf_toread.isEncrypted:
+    if pdf_toread.is_encrypted:
         pdf_toread.decrypt('')
-    pdf_info = pdf_toread.getDocumentInfo()
-    #print(str(pdf_info))
-    # PDF Reference, 3.8.3 Dates, http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/pdf_reference_1-7.pdf
-    # A date is an ASCII string of the form (D:YYYYMMDDHHmmSSOHH'mm')
-    # Examle: D:20170508085336+02'00'
-    raw_date = pdf_info['/CreationDate']
-    #print(str(raw_date))
+
+    pdf_metadata = pdf_toread.metadata
+    print(str(pdf_metadata))
+    raw_date = pdf_metadata['/CreationDate']
     date_str = re.search(r'^D:(\d{14})', raw_date).group(1)
-    #print(str(date_str))
     pdf_date = datetime.strptime(date_str, "%Y%m%d%H%M%S")
-    #print(str(date))
     return pdf_date
 
 # read file from url while respecting redirects and accepting cookies
