7a31a19a6df87c9153f384e564e50c06ca2650fe
bjorndown <bjorndown@smoler.ch>
2025-03-22 11:21:22 +0100

    fix(vm/scraper): also mount pdf archive path.

--- vm/files/debug-scraper.sh
+++ vm/files/debug-scraper.sh
@@ -3,9 +3,11 @@
 podman run --rm -it \
 --mount=type=bind,source="${SSH_AUTH_SOCK}",target="${SSH_AUTH_SOCK}" \
 --mount=type=bind,source=/home/almalinux/scraper/settings.php,target=/opt/lobbywatch/public_html/settings/settings.php,ro \
+--mount=type=bind,source=/home/almalinux/scraper/sql,target=/opt/lobbywatch/sql \
+--mount=type=bind,source=/home/almalinux/scraper/prod_bak,target=/opt/lobbywatch/prod_bak \
+--mount=type=bind,source=/home/almalinux/scraper/archive,target=/opt/lobbywatch/web_scrapers/archive \
 --mount=type=bind,source=/home/almalinux/.ssh/known_hosts,target=/root/.ssh/known_hosts,ro \
 --mount=type=bind,source=/home/almalinux/scraper/my.cnf,target=/root/.my.cnf,ro \
---mount=type=bind,source=/home/almalinux/scraper/sql,target=/opt/lobbywatch/sql \
 --network systemd-scraper \
 --entrypoint sh \
 --privileged \
