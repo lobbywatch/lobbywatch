882c7687e25c3d227fae3739211c237c388be3e5
Björn Mosler <bjorndown@smoler.ch>
2026-02-12 16:28:37 +0100

    pg_create_delta: add test for normalize_organisation

---
+++ web_scrapers/pg_create_delta_test.py
@@ -0,0 +1,17 @@
+import pytest
+from pg_create_delta import normalize_organisation
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (None, None),
+        (" ", ""),
+        (" ()", ""),
+        ("Kindes- und Erwachsenenschutz ()", "Kindes- und Erwachsenenschutz"),
+        ("Enfance et jeunesse (IPEJ)", "Enfance et jeunesse"),
+        ("Climat", "Climat"),
+    ],
+)
+def test_normalize_organisation(input, expected):
+    assert normalize_organisation(input) == expected
