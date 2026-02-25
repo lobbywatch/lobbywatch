e832f4d293be5d89032efe5e04b79b9afecf6b77
Björn Mosler <bjorndown@smoler.ch>
2026-02-25 16:49:27 +0100

    add (failing) tests for four guests

--- web_scrapers/zb_create_delta_test.py
+++ web_scrapers/zb_create_delta_test.py
@@ -396,3 +396,236 @@ def test_sync_parliamentarian_two_guests_first_guest_changes() -> None:
             "insert_person": [call(json_guest(3), batch_time, pdf_date)],
         }
     )
+
+
+def test_sync_parliamentarian_three_guests_add_one_more() -> None:
+    p = {
+        "canton": "bern",
+        "faction": "",
+        "names": ["peter", "parl"],
+        "guests": [json_guest(1), json_guest(2), json_guest(3), json_guest(4)],
+    }
+    mock_db.get_parlamentarier_dict.return_value = {
+        "im_rat_bis": None,
+        "id": "1",
+        "names": ["peter", "parl"],
+    }
+    mock_db.get_guests.return_value = (
+        db_guest(1),
+        db_guest(2),
+        db_guest(3),
+    )
+    mock_db.get_parlamentarier_id_by_names_kanton_fraktion.return_value = "1"
+
+    def get_person_id_mock(conn, names):
+        match names:
+            case ["Guest", "1"] | ["Guest", "2"] | ["Guest", "3"]:
+                return "some_id"
+
+    mock_db.get_person_id.side_effect = get_person_id_mock
+
+    summary_row = sync_parliamentarian(p, conn, batch_time, pdf_date, 0)
+
+    assert summary_row.number == "0"
+    assert summary_row.parlamentarier_id == "1"
+    assert summary_row.parlamentarier_name == "peter parl"
+    assert summary_row.has_changed() is True
+    assert summary_row.get_guest(1).symbol == "="
+    assert summary_row.get_guest(1).id == "guest_1"
+    assert summary_row.get_guest(1).id_old == ""
+    assert summary_row.get_guest(1).changes == ""
+    assert summary_row.get_guest(2).symbol == "="
+    assert summary_row.get_guest(2).id == "guest_2"
+    assert summary_row.get_guest(2).id_old == ""
+    assert summary_row.get_guest(2).changes == ""
+    assert summary_row.get_guest(3).symbol == "="
+    assert summary_row.get_guest(3).id == "guest_3"
+    assert summary_row.get_guest(3).id_old == ""
+    assert summary_row.get_guest(3).changes == ""
+    assert summary_row.get_guest(4).symbol == "+"
+    assert summary_row.get_guest(4).id == ""
+    assert summary_row.get_guest(4).id_old == ""
+    assert summary_row.get_guest(4).changes == ""
+    assert_sql_generator_calls(
+        {
+            "insert_zutrittsberechtigung": [
+                call("1", "", json_guest(4)["function"], batch_time, pdf_date)
+            ],
+            "insert_person": [call(json_guest(4), batch_time, pdf_date)],
+        }
+    )
+
+
+def test_sync_parliamentarian_four_guests_one_leaves() -> None:
+    p = {
+        "canton": "bern",
+        "faction": "",
+        "names": ["peter", "parl"],
+        "guests": [json_guest(1), json_guest(3), json_guest(4)],
+    }
+    mock_db.get_parlamentarier_dict.return_value = {
+        "im_rat_bis": None,
+        "id": "1",
+        "names": ["peter", "parl"],
+    }
+    mock_db.get_guests.return_value = (
+        db_guest(1),
+        db_guest(2),
+        db_guest(3),
+        db_guest(4),
+    )
+    mock_db.get_parlamentarier_id_by_names_kanton_fraktion.return_value = "1"
+    mock_db.get_person_id.return_value = "some_id"
+    summary_row = sync_parliamentarian(p, conn, batch_time, pdf_date, 0)
+
+    assert summary_row.number == "0"
+    assert summary_row.parlamentarier_id == "1"
+    assert summary_row.parlamentarier_name == "peter parl"
+    assert summary_row.has_changed() is True
+    assert summary_row.get_guest(1).symbol == "="
+    assert summary_row.get_guest(1).id == "guest_1"
+    assert summary_row.get_guest(1).id_old == ""
+    assert summary_row.get_guest(1).changes == ""
+    assert summary_row.get_guest(2).symbol == "-"
+    assert summary_row.get_guest(2).id == ""
+    assert summary_row.get_guest(2).id_old == "guest_2"
+    assert summary_row.get_guest(2).changes == ""
+    assert summary_row.get_guest(3).symbol == "="
+    assert summary_row.get_guest(3).id == "guest_3"
+    assert summary_row.get_guest(3).id_old == ""
+    assert summary_row.get_guest(3).changes == ""
+    assert summary_row.get_guest(4).symbol == "="
+    assert summary_row.get_guest(4).id == "guest_4"
+    assert summary_row.get_guest(4).id_old == ""
+    assert summary_row.get_guest(4).changes == ""
+    assert_sql_generator_calls(
+        {
+            "end_zutrittsberechtigung": [call("zb_2", batch_time, pdf_date)],
+        }
+    )
+
+
+def test_sync_parliamentarian_four_guests_last_one_changes() -> None:
+    p = {
+        "canton": "bern",
+        "faction": "",
+        "names": ["peter", "parl"],
+        "guests": [json_guest(1), json_guest(2), json_guest(3), json_guest(5)],
+    }
+    mock_db.get_parlamentarier_dict.return_value = {
+        "im_rat_bis": None,
+        "id": "1",
+        "names": ["peter", "parl"],
+    }
+    mock_db.get_guests.return_value = (
+        db_guest(1),
+        db_guest(2),
+        db_guest(3),
+        db_guest(4),
+    )
+    mock_db.get_parlamentarier_id_by_names_kanton_fraktion.return_value = "1"
+
+    def get_person_id_stub(conn, names):
+        match names:
+            case ("Guest", "1") | ("Guest", "2") | ("Guest", "3") | ("Guest", "4"):
+                return "some_id"
+
+    mock_db.get_person_id.side_effect = get_person_id_stub
+    summary_row = sync_parliamentarian(p, conn, batch_time, pdf_date, 0)
+
+    assert summary_row.number == "0"
+    assert summary_row.parlamentarier_id == "1"
+    assert summary_row.parlamentarier_name == "peter parl"
+    assert summary_row.has_changed() is True
+    assert summary_row.get_guest(1).symbol == "="
+    assert summary_row.get_guest(1).id == "guest_1"
+    assert summary_row.get_guest(1).id_old == ""
+    assert summary_row.get_guest(1).changes == ""
+    assert summary_row.get_guest(2).symbol == "="
+    assert summary_row.get_guest(2).id == "guest_2"
+    assert summary_row.get_guest(2).id_old == ""
+    assert summary_row.get_guest(2).changes == ""
+    assert summary_row.get_guest(3).symbol == "="
+    assert summary_row.get_guest(3).id == "guest_3"
+    assert summary_row.get_guest(3).id_old == ""
+    assert summary_row.get_guest(3).changes == ""
+    assert summary_row.get_guest(4).symbol == "+"
+    assert summary_row.get_guest(4).id == ""
+    assert summary_row.get_guest(4).id_old == "guest_4"
+    assert summary_row.get_guest(4).changes == ""
+    assert_sql_generator_calls(
+        {
+            "end_zutrittsberechtigung": [call("zb_4", batch_time, pdf_date)],
+            "insert_person": [call(json_guest(5), batch_time, pdf_date)],
+            "insert_zutrittsberechtigung": [call("1", batch_time, pdf_date)],
+        }
+    )
+
+
+def test_sync_parliamentarian_two_guests_add_three_new() -> None:
+    p = {
+        "canton": "bern",
+        "faction": "",
+        "names": ["peter", "parl"],
+        "guests": [json_guest(1), json_guest(5), json_guest(6), json_guest(7)],
+    }
+    mock_db.get_parlamentarier_dict.return_value = {
+        "im_rat_bis": None,
+        "id": "1",
+        "names": ["peter", "parl"],
+    }
+    mock_db.get_guests.return_value = (
+        db_guest(1),
+        db_guest(2),
+        db_guest(3),
+        db_guest(4),
+    )
+    mock_db.get_parlamentarier_id_by_names_kanton_fraktion.return_value = "1"
+
+    def get_person_id_stub(conn, names):
+        match names:
+            case ("Guest", "1") | ("Guest", "2") | ("Guest", "3") | ("Guest", "4"):
+                return "some_id"
+
+    mock_db.get_person_id.side_effect = get_person_id_stub
+    summary_row = sync_parliamentarian(p, conn, batch_time, pdf_date, 0)
+
+    assert summary_row.number == "0"
+    assert summary_row.parlamentarier_id == "1"
+    assert summary_row.parlamentarier_name == "peter parl"
+    assert summary_row.has_changed() is True
+    assert summary_row.get_guest(1).symbol == "="
+    assert summary_row.get_guest(1).id == "guest_1"
+    assert summary_row.get_guest(1).id_old == ""
+    assert summary_row.get_guest(1).changes == ""
+    assert summary_row.get_guest(2).symbol == "+"
+    assert summary_row.get_guest(2).id == ""
+    assert summary_row.get_guest(2).id_old == "guest_2"
+    assert summary_row.get_guest(2).changes == ""
+    assert summary_row.get_guest(3).symbol == "+"
+    assert summary_row.get_guest(3).id == ""
+    assert summary_row.get_guest(3).id_old == "guest_3"
+    assert summary_row.get_guest(3).changes == ""
+    assert summary_row.get_guest(4).symbol == "+"
+    assert summary_row.get_guest(4).id == ""
+    assert summary_row.get_guest(4).id_old == "guest_4"
+    assert summary_row.get_guest(4).changes == ""
+    assert_sql_generator_calls(
+        {
+            "end_zutrittsberechtigung": [
+                call("zb_2", batch_time, pdf_date),
+                call("zb_3", batch_time, pdf_date),
+                call("zb_4", batch_time, pdf_date),
+            ],
+            "insert_person": [
+                call(json_guest(5), batch_time, pdf_date),
+                call(json_guest(6), batch_time, pdf_date),
+                call(json_guest(7), batch_time, pdf_date),
+            ],
+            "insert_zutrittsberechtigung": [
+                call("1", "", batch_time, pdf_date),
+                call("1", "", batch_time, pdf_date),
+                call("1", "", batch_time, pdf_date),
+            ],
+        }
+    )
